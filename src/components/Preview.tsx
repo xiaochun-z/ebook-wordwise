e01c78efe74a8f9a0afa02e863d82bfc6c7c27dd
hsiaoshun <io.oi@live.com>
2024-06-29 07:05:14 +0100

    update os based height.

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,10 +1,28 @@
+import { platform } from "@tauri-apps/api/os";
+import { useEffect } from "react";
+
 export default function Preview({ innerHTML }: PreviewProps) {
+  let maxHeight = 215;
+  let clamp = "line-clamp-8";
+  useEffect(() => {
+    const setOSBasedMaxHeight = async () => {
+      const currentPlatform = await platform();
+      if (currentPlatform === "win32") {
+        maxHeight = 215;
+      } else if (currentPlatform == "darwin") {
+        maxHeight = 180;
+        clamp = "line-clamp-6";
+      }
+    };
+    setOSBasedMaxHeight();
+  }, []);
+
   return (
     <div
       dangerouslySetInnerHTML={{ __html: innerHTML }}
-      style={{ maxHeight: "215px", overflow: "hidden" }}
+      style={{ maxHeight: maxHeight + "px", overflow: "hidden" }}
       id="preview"
-      className="overflow-ellipsis bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4 font-normal text-gray-700 dark:text-gray-400 line-clamp-8"
+      className={`overflow-ellipsis bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4 font-normal text-gray-700 dark:text-gray-400 ${clamp}`}
     ></div>
   );
 }
